6d42c8d70f8644ca365e8dc1ab0f949e1a2b56e9
nic <nicwoods63@gmail.com>
2020-03-30 17:51:11 -0500

    First stab at header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <div className="header">
+      <div className="header-box">
+        <p className="header-nw">NW</p>
+        <p className="header-name">Nic Woods</p>
+      </div>
+    </div>
+  );
+}
+export default Header;
---
+++ src/components/Sun.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Sun = () => (
+  <div className="sun">
+  <div className="sun-body"></div>
+  </div>
+);
+export default Sun;
