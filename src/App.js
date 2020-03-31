7058989f9b16ae971908ebf3e51fcec744454643
nic <nicwoods63@gmail.com>
2020-03-30 21:23:10 -0500

    Spinning header, and new body component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Sun from './components/Sun';
 import Header from './components/Header';
+import Body from './components/Body';
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <Body />
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Body() {
+  return(
+    <div className="body">
+    Hi, I'm Nic. I like to make things.
+    </div>
+  );
+}
+export default Body;
